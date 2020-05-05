992cabd51cd6f8a79f5ca71d1fe3f8a31b5db0d4
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-05 20:00:08 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
     name = gets.chomp until name == "exit"
   end 
  
-  def list_songs(x)
+  def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |song, index|
       puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
